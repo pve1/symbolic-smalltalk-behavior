a375c320f09345ab0db5f65f26b6cc4ac23f0501
pve1 <pve1@foo.invalid>
2023-05-31 17:30:02 +0300

    Initial commit.

---
+++ symbolic-smalltalk-behavior.asd
@@ -0,0 +1,9 @@
+(asdf:defsystem :symbolic-smalltalk-behavior
+  :description "Provides Symbolic-smalltalk class behavior."
+  :author "Peter von Etter"
+  :license "LGPL-3.0"
+  :version "0.0.1"
+  :serial t
+  :components ((:file "symbolic-smalltalk-behavior"))
+  :depends-on (#:symbolic-smalltalk-core
+               #:alexandria))
---
+++ symbolic-smalltalk-behavior.lisp
@@ -0,0 +1,75 @@
+(defpackage :symbolic-smalltalk-behavior
+  (:local-nicknames (:core :symbolic-smalltalk-core))
+  (:use :cl))
+
+(in-package :symbolic-smalltalk-behavior)
+
+;;; Object
+
+(core:define-method (core:object class)
+  (core:class self))
+
+(core:define-method (core:object :does-not-understand message)
+  (error 'core:message-not-understood
+         :recipient self
+         :message message))
+
+;;; Behavior
+
+(core:define-method (core:behavior new)
+  (core:new self))
+
+(core::define-method (core:behavior basic-new)
+  (core:basic-new self))
+
+(core:define-method (core:behavior instance-variables)
+  (core:instance-variables self))
+
+(core:define-method (core:behavior class-variables)
+  (core:class-variables self))
+
+;; Object class :add-selector '(:x x :y y)
+;;              :with-lambda '(lambda (self x y) ...)
+
+(core:define-method (core:behavior :add-selector selector
+                                   :with-lambda lambda-form)
+  (core:add-method-to-class self selector lambda-form))
+
+(core:define-method (core:behavior :remove-selector selector)
+  (core:remove-method-from-class self selector))
+
+(core:define-method (core:behavior :add-slot-accessor slot-name)
+  (core:add-slot-accessor self slot-name))
+
+(core:define-method (core:class :subclass name)
+  (core:subclass self name))
+
+(core:define-method (core:class :subclass name
+                                :instance-variable-names instance-variables
+                                :class-variable-names class-variables)
+  (core:subclass self name
+                 :instance-variables instance-variables
+                 :class-variables class-variables))
+
+;;; Message
+
+(core:define-method (core:message recipient)
+  (core:message-recipient self))
+
+(core:define-method (core:message :recipient recipient)
+  (setf (core:message-recipient self) recipient)
+  self)
+
+(core:define-method (core:message selector)
+  (core:message-selector self))
+
+(core:define-method (core:message :selector selector)
+  (setf (core:message-selector self) selector)
+  self)
+
+(core:define-method (core:message arguments)
+  (core:message-arguments self))
+
+(core:define-method (core:message :arguments arguments)
+  (setf (core:message-arguments self) arguments)
+  self)
